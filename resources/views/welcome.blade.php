896aa5ffb23076778ed672f44eb9285c06d41a79
jsanchez23 <jsanchez@airavirtual.com>
2021-07-14 19:48:27 -0500

    inicio del proyecto

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link rel="stylesheet" href="{{asset("css/remote_control.css")}}">
+    <link href="{{asset('assets/bower/materialize/dist/css/materialize.min.css')}}" type="text/css" rel="stylesheet" media="screen,projection"/>
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<body>
+
+<!--JavaScript at end of body for optimized loading-->
+<script type="text/javascript" src="js/materialize.min.js"></script>
+</body>
+</html>
+
+
---
+++ webpack.mix.js
@@ -0,0 +1,37 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/app.js', 'public/js')
+    .postCss('resources/css/app.css', 'public/css', [
+        //
+    ]);
+
+mix.sass('resources/sass/remote_control.scss', 'public/css');
+
+const WebpackShellPlugin = require('webpack-shell-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const fs = require('fs');
+const gracefulFs = require('graceful-fs');
+gracefulFs.gracefulify(fs);
+
+mix.webpackConfig({
+    plugins:
+        [
+            new CopyWebpackPlugin(
+                {
+                    patterns: [
+                        { from: 'node_modules/materialize-css/', to: 'assets/bower/materialize/'  }
+                    ]
+                })
+        ]
+});
